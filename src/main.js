6cfa1adf32d9d0c25fa6fe996b6a819ad72dd199
王英杰 <gc_wangyingjie@ieggtc.com>
2024-11-28 11:03:14 +0800

    去除无关代码

--- src/main.js
+++ src/main.js
@@ -5,22 +5,13 @@ const path = require('path');
 const pluginPath = LiteLoader.plugins.AutoForward.path.plugin;
 const configTemplatePath = path.join(pluginPath, 'src/config/config.json');
 let userConfigPath;
-// 创建窗口时触发
-exports.onBrowserWindowCreated = (window) => {
-    // window 为 Electron 的 BrowserWindow 实例
-    
-}
 
 
-// 用户登录时触发
 exports.onLogin = (uid) => {
-    // uid 为 账号 的 字符串 标识
     userConfigPath = path.join(pluginPath, `src/config/config_${uid}.json`);
-    console.log(`[AutoForward LOG] 用户 ${uid} 登录，配置文件路径为 ${userConfigPath}`)
     if (!fs.existsSync(userConfigPath)) {
         fs.copyFileSync(configTemplatePath, userConfigPath);
-        console.log(`[AutoForward LOG] 创建用户配置文件 ${userConfigPath}`)
-    };
+    }
 }
 
 ipcMain.on('AutoForward.log', (event, ...msg) => {
@@ -28,10 +19,5 @@ ipcMain.on('AutoForward.log', (event, ...msg) => {
 })
 
 ipcMain.on('AutoForward.getConfig', (event, config) => {
-    console.log('[AutoForward LOG]', config, userConfigPath)
     fs.writeFileSync(userConfigPath, JSON.stringify(config, null, '\t'))
-    // const config = JSON.parse(fs.readFileSync('config/config.json'))
-    // event.sender.send('AutoForward.configResult', config)
-    // console.log(config)
-    // console.log('---------------')
 })
